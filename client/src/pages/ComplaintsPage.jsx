bb4ecbe3f03e9fba1747931d0ce02bf8420e25ea
balajiiyer12 <0127balaji@gmail.com>
2026-09-09 12:01:03 +0530

    minor fix

--- client/src/pages/ComplaintsPage.jsx
+++ client/src/pages/ComplaintsPage.jsx
@@ -25,10 +25,7 @@ export default function ComplaintsPage() {
       setError("");
       const response = await fetch(`${API_BASE_URL}/complaints/tenant`, {
         method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        credentials: "include"
       });
 
       const data = await response.json();
