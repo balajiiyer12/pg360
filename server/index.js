92ef5e5ba59d43bf47bbcdd189b6f85f6d72a7f9
balajiiyer12 <0127balaji@gmail.com>
2026-09-15 23:45:49 +0530

    done

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ try {
 
   app.use(
     cors({
-      origin: ["http://storage.googleapis.com","http://localhost:5173"] ,
+      origin: ["http://storage.googleapis.com"] ,
       credentials: true,
     })
   );
